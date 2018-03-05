925da2610bbc33a8d060eb2ea213ef67ccab0026
relson816 <relson.batayola@gmail.com>
2018-03-05 22:56:11 +0800

    Retrieved a collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,24 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+interface Post {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
   constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
-
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
   }
 }
