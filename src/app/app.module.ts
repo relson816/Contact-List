dd96ea7d6a6a33f73f9891099c8b6bf42c842cc2
relson816 <relson.batayola@gmail.com>
2018-03-05 22:52:36 +0800

    Integrated Firestore into Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDNu1nRIRKt52562aOPcHAMYgMddXgb624",
+    authDomain: "firestore-573fd.firebaseapp.com",
+    databaseURL: "https://firestore-573fd.firebaseio.com",
+    projectId: "firestore-573fd",
+    storageBucket: "firestore-573fd.appspot.com",
+    messagingSenderId: "823637704398"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  
+    AngularFirestoreModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
